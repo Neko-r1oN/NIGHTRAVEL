16fc045b43f73ded879458f9d632236bc1b62040
S-JOBI23-052\student <231116@st.yoshida-g.ac.jp>
2025-05-08 11:07:19 +0900

    ファイルを追加・シーン移動追加

---
+++ Client/Assets/99_User/01_Ueno/02_Script/ResultManager.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RisultManager : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonUp(0) || Input.GetKeyUp(KeyCode.Return))
+        {
+            SceneManager.LoadScene("Title Ueno");
+        }
+    }
+}
