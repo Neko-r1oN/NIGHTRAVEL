a332fdc1831d5f4162b014c742abe1f81bbafbb3
S-JOBI23-062\student <231049@st.yoshida-g.ac.jp>
2025-05-12 11:13:00 +0900

    ステージの物体を調整、テスト用のキャラクターのスクリプトを編集

--- Client/Assets/99_User/05_Miura/02_Script/TestCharacter.cs
+++ Client/Assets/99_User/05_Miura/02_Script/TestCharacter.cs
@@ -3,78 +3,43 @@ using UnityEngine.Audio;
 
 public class TestCharacter : MonoBehaviour
 {
-    //�}�b�v�̎g�p�����m���߂邽�߂̃L�����N�^�[�̃X�N���v�g
+    //テスト用キャラクターのスクリプト
     //Aouther:Yuki Miura(y-miura)
 
-    [SerializeField] LayerMask blockLayer;
-
-    Rigidbody2D rigidbody2d;
-    float speed;
-    float jumpPower = 480;
-
-    public enum DIRECTION_TYPE
-    {
-        STOP, RIGHT, LEFT,
-    }
-    DIRECTION_TYPE directionType = DIRECTION_TYPE.STOP;
-
+    // オブジェクト・コンポーネント参照
+    private Rigidbody2D rigidbody2D; // Rigidbody2Dコンポーネントへの参照
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        rigidbody2d = GetComponent<Rigidbody2D>();
+        // コンポーネント参照取得
+        rigidbody2D = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        //�����L�[�擾
-        float x = Input.GetAxis("Horizontal");
-
-        if (x == 0)//���͂�����������~�܂�
-        {
-            directionType = DIRECTION_TYPE.STOP;
+        // 仮の移動処理
+        if (Input.GetKey(KeyCode.D))
+        {// 右方向の移動入力
+            Vector2 pos = transform.position;
+            pos.x += 0.05f;
+            transform.position = pos;
         }
-        else if (x > 0)//�E�ɐi��
-        {
-            directionType = DIRECTION_TYPE.RIGHT;
-        }
-        else if (x < 0)//���ɐi��
-        {
-            directionType = DIRECTION_TYPE.LEFT;
+        else if (Input.GetKey(KeyCode.A))
+        {// 左方向の移動入力
+            Vector2 pos = transform.position;
+            pos.x -= 0.05f;
+            transform.position = pos;
         }
 
-        void FixedUpdate()
-        {
-          
-
-            switch (directionType)
-            {
-                case DIRECTION_TYPE.STOP:
-                    speed = 0;
-                    break;
-
-                case DIRECTION_TYPE.RIGHT:
-                    speed = 5;
-                    transform.localScale = new Vector3(1, 1, 1);
-                    break;
-
-                case DIRECTION_TYPE.LEFT:
-                    speed = -5;
-                    transform.localScale = new Vector3(-1, 1, 1);
-
-                    break;
-            }
-
-            //���x���㏑��������@
-            rigidbody2d.linearVelocity = new Vector2(speed, rigidbody2d.linearVelocity.y);
-
-        }
-
-        void Jump()
-        {
-            rigidbody2d.AddForce(Vector2.up * jumpPower);
-            transform.localPosition += new Vector3(0, 0.1f, 0);
+        // ジャンプ操作
+        if (Input.GetKeyDown(KeyCode.Space))
+        {// ジャンプ開始
+         // ジャンプ力を計算
+            float jumpPower = 15.0f;
+            // ジャンプ力を適用
+            rigidbody2D.linearVelocity = new Vector2(rigidbody2D.linearVelocity.x, jumpPower);
         }
     }
 }
