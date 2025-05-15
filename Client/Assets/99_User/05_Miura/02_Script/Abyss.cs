536b388cc7b376c3ac44ac1fa7865404f64fbf88
S-JOBI23-062\student <231049@st.yoshida-g.ac.jp>
2025-05-15 09:33:18 +0900

    トラップ用のスクリプトを追加して、スクリプトを編集

--- Client/Assets/99_User/05_Miura/02_Script/Abyss.cs
+++ Client/Assets/99_User/05_Miura/02_Script/Abyss.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class Abyss : MonoBehaviour
+public class Abyss : Trap
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
