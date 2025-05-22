3bc81f7b7503873f7a784dd879ebefe01a7ab72d
S-JOBI23-062\student <231049@st.yoshida-g.ac.jp>
2025-05-22 12:08:22 +0900

    封鎖済みドアのスクリプトを編集し、キャラクターが触れた状態で特定のキーを押すとドアを消すようにした

--- Client/Assets/99_User/05_Miura/02_Script/SealedDoor.cs
+++ Client/Assets/99_User/05_Miura/02_Script/SealedDoor.cs
@@ -2,17 +2,23 @@ using UnityEngine;
 
 public class SealedDoor : MonoBehaviour
 {
-    private GameObject door;
+    public bool isDoor;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        isDoor = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //ドアに触れたら&&Yキー(仮)を押したら
+        if (isDoor == false && Input.GetKeyDown(KeyCode.X))
+        {
+            //ドア破壊
+            this.gameObject.SetActive(false);
+        }
     }
 
     public void OnCollisionEnter2D(Collision2D collision)
@@ -20,6 +26,7 @@ public class SealedDoor : MonoBehaviour
         if (collision.gameObject.CompareTag("Player"))
         {
             Debug.Log("ドアに触れる");
+            isDoor = false;
         }
     }
 }
