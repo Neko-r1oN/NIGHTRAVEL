b4a51f079810b3d01a8f05f339eebd41ee4691f0
Miura\student <231049@st.yoshida-g.ac.jp>
2025-06-04 15:57:19 +0900

    プレイヤーのダメージ計算(途中)

--- Client/Assets/99_User/05_Miura/02_Script/Abyss.cs
+++ Client/Assets/99_User/05_Miura/02_Script/Abyss.cs
@@ -2,10 +2,14 @@ using UnityEngine;
 
 public class Abyss : MonoBehaviour
 {
+    SampleChara_Copy sampleChara_Copy = new SampleChara_Copy();
+    //float characterHP;
+    //int damage
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+       
     }
 
     // Update is called once per frame
@@ -16,9 +20,10 @@ public class Abyss : MonoBehaviour
 
     public void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("Player"))
+        if(collision.gameObject.CompareTag("Player"))
         {
             Debug.Log("ÉvÉåÉCÉÑÅ[Ç™ìﬁóéÇ…óéÇøÇΩ");
+            //sampleChara_Copy.ApplyDamage(damage,this.transform.position);
         }
         if(collision.gameObject.CompareTag("Enemy"))
         {
