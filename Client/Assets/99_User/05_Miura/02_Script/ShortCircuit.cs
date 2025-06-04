82d987372f0e76448eab23f2aeb3739d0d070aa7
Miura\student <231049@st.yoshida-g.ac.jp>
2025-06-04 13:38:38 +0900

    「Shortcircuit」スクリプトに、プレイヤーが当たったらエフェクトをInstantiateする処理を追加。コンソールに文字を表示する処理を削除

--- Client/Assets/99_User/05_Miura/02_Script/ShortCircuit.cs
+++ Client/Assets/99_User/05_Miura/02_Script/ShortCircuit.cs
@@ -5,6 +5,7 @@ public class ShortCircuit : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
 
+    [SerializeField] GameObject ElectronicEffect;
 
     void Start()
     {
@@ -21,12 +22,11 @@ public class ShortCircuit : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("Player"))
         {
-            Debug.Log("プレイヤーが漏電フィールドに当たった");
-            
+            Instantiate(ElectronicEffect,this.transform.position,this.transform.rotation);
         }
         if(collision.gameObject.CompareTag("Enemy"))
         {
-            Debug.Log("敵が漏電フィールドに当たった");
+
         }
     }
 }
