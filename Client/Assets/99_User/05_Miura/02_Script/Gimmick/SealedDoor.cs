4769bf3c8266f5a331c9411f4325d346cfd443c1
Miura\student <231049@st.yoshida-g.ac.jp>
2025-06-16 15:52:55 +0900

    プレイヤーがドアに触れずに攻撃した時にもドアを破壊できるように、一時的に「Attack」タグを追加。また、「「Attack」タグが付いたものが触れたらドアを破壊する」処理を追加。「DoorFrag」レイヤーを追加して、ドアの破壊エフェクトに付与

--- Client/Assets/99_User/05_Miura/02_Script/Gimmick/SealedDoor.cs
+++ Client/Assets/99_User/05_Miura/02_Script/Gimmick/SealedDoor.cs
@@ -1,15 +1,22 @@
+using System.Collections.Generic;
 using System.ComponentModel;
+using Unity.VisualScripting;
 using UnityEngine;
+using static Unity.VisualScripting.Member;
 
 public class SealedDoor : MonoBehaviour
 {
-    [SerializeField] GameObject ExplosionEffect;
+    [SerializeField] GameObject DoorFragment;
+    GameObject player;
+    List<Transform> fragmentList=new List<Transform>();
+    Rigidbody2D rigidbody2D;
     public bool isDoor;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         isDoor = false;
+        rigidbody2D=this.gameObject.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -18,9 +25,23 @@ public class SealedDoor : MonoBehaviour
         //ドアに触れていて、攻撃したら
         if (isDoor == true && Input.GetKeyDown(KeyCode.X))
         {
+            GameObject fragment;
+            fragment= Instantiate(DoorFragment, new Vector2(this.transform.position.x, this.transform.position.y - 2), this.transform.rotation);
+
+            for (int i = 0; i < fragment.transform.childCount; i++)
+            {
+                if(this.transform.position.x- player.transform.position.x>=0)
+                {
+                    fragment.transform.GetChild(i).GetComponent<Rigidbody2D>().AddForce(new Vector2(1000, 200));
+                }
+                else
+                {
+                    fragment.transform.GetChild(i).GetComponent<Rigidbody2D>().AddForce(new Vector2(-1000, -200));
+                }
+            }
+
             //ドアを壊す
             Destroy(this.gameObject);
-            Instantiate(ExplosionEffect, this.transform.position, this.transform.rotation);
         }
     }
 
@@ -28,7 +49,34 @@ public class SealedDoor : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("Player"))
         {
+            player=collision.gameObject;
             isDoor = true;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.CompareTag("Attack"))
+        {
+            player = collision.gameObject;
+            GameObject fragment;
+            fragment = Instantiate(DoorFragment, new Vector2(this.transform.position.x, this.transform.position.y - 2), this.transform.rotation);
+
+            for (int i = 0; i < fragment.transform.childCount; i++)
+            {
+                if (this.transform.position.x - player.transform.position.x >= 0)
+                {
+                    fragment.transform.GetChild(i).GetComponent<Rigidbody2D>().AddForce(new Vector2(1000, 200));
+                }
+                else
+                {
+                    fragment.transform.GetChild(i).GetComponent<Rigidbody2D>().AddForce(new Vector2(-1000, -200));
+                }
+            }
+
+            //ドアを壊す
+            Destroy(this.gameObject);
+
         }
     }
 
