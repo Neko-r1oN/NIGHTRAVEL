1bb6f3190e2dfcde95b0e0f7f6eaf5a88456c498
Miura\student <231049@st.yoshida-g.ac.jp>
2025-06-17 11:23:47 +0900

    プレイヤーがダメージを与える処理のDoDashDamage関数内を編集

--- Client/Assets/99_User/04_Nakamoto/02_Script/Player/Characters/Sword.cs
+++ Client/Assets/99_User/04_Nakamoto/02_Script/Player/Characters/Sword.cs
@@ -76,7 +76,6 @@ public class Sword : PlayerBase
     {
         power = Mathf.Abs(power);
         Collider2D[] collidersEnemies = Physics2D.OverlapCircleAll(attackCheck.position, k_AttackRadius);
-
         for (int i = 0; i < collidersEnemies.Length; i++)
         {
             if (collidersEnemies[i].gameObject.tag == "Enemy")
@@ -87,16 +86,12 @@ public class Sword : PlayerBase
                 }
                 //++ GetComponentでEnemyスクリプトを取得し、ApplyDamageを呼び出すように変更
                 //++ 破壊できるオブジェを作る際にはオブジェの共通被ダメ関数を呼ぶようにする
-
-                if(collidersEnemies[i].gameObject.tag == "Enemy")
-                {
-                    collidersEnemies[i].gameObject.GetComponent<EnemyBase>().ApplyDamage(power, playerPos);
-                }
-                else if(collidersEnemies[i].gameObject.tag == "Object")
-                {
-                    collidersEnemies[i].gameObject.GetComponent<ObjectBase>().ApplyDamage();
-                }
+                collidersEnemies[i].gameObject.GetComponent<EnemyBase>().ApplyDamage(power, playerPos);
                 cam.GetComponent<CameraFollow>().ShakeCamera();
+            }
+            else if (collidersEnemies[i].gameObject.tag == "Object")
+            {
+                collidersEnemies[i].gameObject.GetComponent<ObjectBase>().ApplyDamage();
             }
         }
     }
