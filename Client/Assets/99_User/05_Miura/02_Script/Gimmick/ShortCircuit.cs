e29e816ffc8e179697cbf9d4cdb0ce2d7b66344b
Miura\student <231049@st.yoshida-g.ac.jp>
2025-06-18 23:43:48 +0900

    「ShortCircuit」スクリプトからStart関数とUpdate関数を削除。コメントを追加

--- Client/Assets/99_User/05_Miura/02_Script/Gimmick/ShortCircuit.cs
+++ Client/Assets/99_User/05_Miura/02_Script/Gimmick/ShortCircuit.cs
@@ -9,33 +9,36 @@ public class ShortCircuit : MonoBehaviour
     EnemyBase enemyBase;
     Vector2 pos = Vector2.zero;
 
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
+    /// <summary>
+    /// プレイヤーのダメージ量計算処理
+    /// </summary>
     void HitPlayerDamage()
     {
+        //プレイヤーの最大HPを代入
         int maxHP = playerBase.MaxHP;
 
+        //ダメージ量は最大HPの5割
         int damage = Mathf.FloorToInt(maxHP * 0.05f);
         playerBase.ApplyDamage(damage);
-        Debug.Log(playerBase.HP);
     }
+
+    /// <summary>
+    /// 敵のダメージ量計算処理
+    /// </summary>
     void HitEnemyDamage()
     {
+        //敵の最大HPを代入
         int maxLife = enemyBase.MaxHP;
 
+        //ダメージ量は最大HPの5割
         int damage = Mathf.FloorToInt(maxLife * 0.05f);
         enemyBase.ApplyDamage(damage);
     }
 
+    /// <summary>
+    /// 漏電フィールドでダメージを与える処理
+    /// </summary>
+    /// <param name="collision">ぶつかったオブジェクト</param>
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
@@ -45,12 +48,15 @@ public class ShortCircuit : MonoBehaviour
         }
         if (collision.gameObject.CompareTag("Enemy"))
         {
-            //Debug.Log("敵が漏電フィールドに当たった");
             enemyBase = collision.gameObject.GetComponent<EnemyBase>();
             InvokeRepeating("HitEnemyDamage", 0.1f, 0.5f);
         }
     }
 
+    /// <summary>
+    /// 漏電フィールドを離れた時の処理
+    /// </summary>
+    /// <param name="collision">触れたオブジェクト</param>
     private void OnTriggerExit2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
