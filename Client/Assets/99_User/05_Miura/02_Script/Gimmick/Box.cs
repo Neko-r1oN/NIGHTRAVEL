4f9e051b9b5341e64df12018bb22b8ad079976eb
Miura\student <231049@st.yoshida-g.ac.jp>
2025-07-03 13:27:26 +0900

    箱を一定間隔で生成させる処理、「Abyss」タグのものに触れたら箱を消す処理を「Box」スクリプトに追加。破片を生成する位置を変更

--- Client/Assets/99_User/05_Miura/02_Script/Gimmick/Box.cs
+++ Client/Assets/99_User/05_Miura/02_Script/Gimmick/Box.cs
@@ -10,10 +10,48 @@ using UnityEngine;
 
 public class Box : ObjectBase
 {
+    [SerializeField] GameObject BoxPrefab;  //箱プレハブ取得
     [SerializeField] GameObject BoxFragment;　//破片エフェクトを取得
     PlayerBase player;
+
     bool isBroken = false;
 
+    void Start()
+    {
+        //SpawnBox関数を繰り返し呼び出して、箱を繰り返し生成する
+        InvokeRepeating("SpawnBox", 5.5f, 10);
+    }
+
+    /// <summary>
+    /// 箱を生成する処理
+    /// </summary>
+    public void SpawnBox()
+    {
+        GameObject boxObj = BoxPrefab;
+
+        //箱を生成する
+        Instantiate(boxObj, new Vector2(28.0f, 27.0f), Quaternion.identity);
+    }
+
+    /// <summary>
+    /// 箱を消す処理
+    /// </summary>
+    /// <param name="collision">触れたオブジェクト</param>
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //触れたもののタグが「Abyss」だったら
+        if(collision.CompareTag("Abyss"))
+        {
+            GameObject boxPrefab = BoxPrefab;
+
+            //箱を消す
+            Destroy(boxPrefab);
+        }
+    }
+
+    /// <summary>
+    /// 箱を壊したときの処理
+    /// </summary>
     public override void ApplyDamage()
     {
         if (isBroken == true)
@@ -25,7 +63,9 @@ public class Box : ObjectBase
         player = GameObject.FindWithTag("Player").GetComponent<PlayerBase>();
 
         GameObject fragment; //破片のオブジェクト
-        fragment = Instantiate(BoxFragment, new Vector2(this.transform.position.x, this.transform.position.y - 1.5f), this.transform.rotation); //破片オブジェクトを生成(position.xはドアの位置、yはドアより少し下の位置)
+
+        //破片オブジェクトを生成(position.xは箱の位置、yは箱より少し上の位置)
+        fragment = Instantiate(BoxFragment, new Vector2(this.transform.position.x, this.transform.position.y + 1.5f), this.transform.rotation);
 
         for (int i = 0; i < fragment.transform.childCount; i++)
         {//fragmentの子の数だけループ
@@ -40,21 +80,33 @@ public class Box : ObjectBase
             FadeFragment(fragment.transform.GetChild(i));
         }
 
-        Destroy(this.gameObject);//箱を壊す
+        //箱を壊す
+        Destroy(this.gameObject);
+
+        //破片を消す
         DestroyFragment(fragment.transform);
     }
 
+    /// <summary>
+    /// 破片をフェードアウトさせる処理
+    /// </summary>
+    /// <param name="fragment">破片プレハブ</param>
     public override void FadeFragment(Transform fragment)
     {
         if (fragment == null)
         {
+            //何もしない
             return;
         }
 
+        //6秒かけて破片をフェードアウトさせる
         fragment.GetComponent<Renderer>().material.DOFade(0, 6);
-
     }
 
+    /// <summary>
+    /// 破片を消す処理
+    /// </summary>
+    /// <param name="fragment">破片プレハブ</param>
     public async void DestroyFragment(Transform fragment)
     {
         await Task.Delay(6000);
