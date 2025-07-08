95da4d8450df200884940a0332a3339bd0999f1d
KosukeKida <231122@st.yoshida-g.ac.jp>
2025-07-08 11:53:35 +0900

    クライアントに制作環境を構築

--- NIGHTRAVEL.Shared/Interfaces/Model/Entity/User.cs
+++ NIGHTRAVEL.Shared/Interfaces/Model/Entity/User.cs
@@ -6,17 +6,24 @@
 ///
 ////////////////////////////////////////////////////////////////
 
+using MessagePack;
 using System;
 
 namespace NIGHTRAVEL.Shared.Interfaces.Model.Entity
 {
+    [MessagePackObject]
     /// <summary>
     /// ユーザーのカラム設定(Public)
     /// </summary>
     public class User
     {
-        public int id { get; set; }                         //ユーザーのid
+        [Key(0)]
+        public int Id { get; set; }                         //ユーザーのid
+        [Key(1)]
+        public string Name { get; set; }                    //名前
+        [Key(2)]
         public DateTime Created_at { get; set; }            //生成日時
+        [Key(3)]
         public DateTime Updated_at { get; set; }            //更新日時
 
     }
