557a80953d300276f1ce753052af21131febb918
Miura\student <231049@st.yoshida-g.ac.jp>
2025-07-15 13:59:25 +0900

    ベルトコンベアーの歯車のイラストを追加。歯車の処理を行う「Gear」スクリプトを新規追加して編集。「GimmickScene」を編集

---
+++ Client/Assets/99_User/05_Miura/02_Script/Gimmick/Gear.cs
@@ -0,0 +1,28 @@
+//==============================
+// ベルトコンベアーの歯車のスクリプト
+// Aouther:y-miura
+// Date:2025/07/15
+//==============================
+
+using DG.Tweening;
+using UnityEngine;
+
+public class Gear : MonoBehaviour
+{
+    public float rotZ; //歯車の回転方向(Z軸)
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        transform.DOLocalRotate(new Vector3(0, 0, rotZ), 0.8f, RotateMode.FastBeyond360)
+        .SetEase(Ease.Linear)
+        .SetLoops(-1, LoopType.Restart);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
