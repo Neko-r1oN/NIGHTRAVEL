8b10d7f054b634b9e0c23c8fd95b2673940f0e95
k-kawaguchi <231076@st.yoshida.-g.ac.jp>
2025-07-16 10:04:54 +0900

    Merge用コミット

---
+++ Client/Assets/50_Audio/Scripts/SEPath.cs
@@ -0,0 +1,16 @@
+﻿namespace KanKikuchi.AudioManager{
+
+/// <summary>
+/// SEファイルへのパスを定数で管理するクラス
+/// </summary>
+public static class SEPath{
+
+	public const string JINGLE10    = "SE/Jingle/jingle10";
+	public const string SYSTEM20    = "SE/system20";
+	public const string SYSTEM23    = "SE/system23";
+	public const string TITLE_NOISE = "SE/01_Title/Title_Noise";
+	public const string TITLE_WIND  = "SE/01_Title/Title_Wind";
+
+}
+
+}
---
+++ Client/Assets/99_User/03_Kawaguchi/02_Script/01_Title/TitleManagerk.cs
@@ -0,0 +1,126 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using DG.Tweening;                   //DOTweenを使うときはこのusingを入れる
+using KanKikuchi.AudioManager;
+
+public class TitleManagerk : MonoBehaviour
+{
+
+    [SerializeField] GameObject fade;
+   // [SerializeField] GameObject menu;
+
+    public static bool isMenuFlag;
+
+    bool isSuccess;
+
+    void Start()
+    {
+        fade.SetActive(true);               //フェードを有効化
+      //  menu.SetActive(false);              //メニューを非表示
+        isMenuFlag = false;                 //メニューフラグを無効化
+
+        //ローカルのユーザーデータを取得
+       // isSuccess = UserModel.Instance.LoadUserData();
+
+        //ローカルのユーザーデータを取得
+       // isSuccess = OptionModel.Instance.LoadOptionData();
+
+        //BGM再生
+        BGMManager.Instance.Play(
+            audioPath: BGMPath.TITLE, //再生したいオーディオのパス
+            volumeRate: 0.5f,                //音量の倍率
+            delay: 1.0f,                //再生されるまでの遅延時間
+            pitch: 1,                //ピッチ
+            isLoop: true,             //ループ再生するか
+            allowsDuplicate: false             //他のBGMと重複して再生させるか
+        );
+
+        SEManager.Instance.Play(
+           audioPath: SEPath.TITLE_WIND, //再生したいオーディオのパス
+           volumeRate: 0.4f,                //音量の倍率
+           delay: 0,                //再生されるまでの遅延時間
+           pitch: 1,                //ピッチ
+           isLoop: true,             //ループ再生するか
+           callback: null              //再生終了後の処理
+        );
+
+        SEManager.Instance.Play(
+           audioPath: SEPath.TITLE_NOISE, //再生したいオーディオのパス
+           volumeRate: 0.4f,                //音量の倍率
+           delay: 0,                //再生されるまでの遅延時間
+           pitch: 1,                //ピッチ
+           isLoop: true,             //ループ再生するか
+           callback: null              //再生終了後の処理
+        );
+
+        //全てのBGMをフェードイン
+        BGMManager.Instance.FadeIn(1.0f);
+        //全てのSEをフェードイン
+        SEManager.Instance.FadeIn(13.0f);
+    }
+
+    public void OpenOptionButton()
+    {
+        ClickSE();
+
+       // menu.SetActive(true);
+        isMenuFlag = true;
+        
+    }
+
+    public void CloseOptionButton()
+    {
+        SEManager.Instance.Play(
+            audioPath: SEPath.SYSTEM20, //再生したいオーディオのパス
+            volumeRate: 1,                //音量の倍率
+            delay: 0,                //再生されるまでの遅延時間
+            pitch: 1,                //ピッチ
+            isLoop: false,             //ループ再生するか
+            callback: null              //再生終了後の処理
+        );
+
+        isMenuFlag = false;
+        Invoke("CloseMenu", 0.5f);
+        
+    }
+
+    void CloseMenu()
+    {
+       // menu.SetActive(false);
+    }
+
+    public void OnClickStart()
+    {
+        ClickSE();
+
+        //全てのBGMをフェードアウト
+        BGMManager.Instance.FadeOut(BGMPath.TITLE, 3, () => {
+            Debug.Log("BGMフェードアウト終了");
+        });
+        SEManager.Instance.FadeOut(SEPath.TITLE_WIND, 3, () => {
+            Debug.Log("BGMフェードアウト終了");
+        });
+        SEManager.Instance.FadeOut(SEPath.TITLE_NOISE, 3, () => {
+            Debug.Log("BGMフェードアウト終了");
+        });
+    }
+
+    /// <summary>
+    /// クリック音再生関数
+    /// </summary>
+    public void ClickSE()
+    {
+        SEManager.Instance.Play(
+            audioPath: SEPath.SYSTEM20, //再生したいオーディオのパス
+            volumeRate: 1,                //音量の倍率
+            delay: 0,                //再生されるまでの遅延時間
+            pitch: 1,                //ピッチ
+            isLoop: false,             //ループ再生するか
+            callback: null              //再生終了後の処理
+        );
+    }
+
+}
+
