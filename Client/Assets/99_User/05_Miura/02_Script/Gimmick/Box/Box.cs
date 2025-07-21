4565e366679193f10596307412afdb31efc99d95
Miura\student <231049@st.yoshida-g.ac.jp>
2025-07-22 01:38:06 +0900

    「Scripts」ファイルに箱関連のスクリプトを格納する「Box」フォルダを追加。「BoxManager」スクリプトを追加して編集。「Box」スクリプトと「BoxManager」スクリプトを「Box」フォルダに移行。

--- Client/Assets/99_User/05_Miura/02_Script/Gimmick/Box/Box.cs
+++ Client/Assets/99_User/05_Miura/02_Script/Gimmick/Box/Box.cs
@@ -1,7 +1,7 @@
 //====================
-//足場とする箱のスクリプト
-//Aouther:y-miura
-//Date:2025/07/01
+// 足場とする箱のスクリプト
+// Aouther:y-miura
+// Date:2025/07/01
 //====================
 
 using DG.Tweening;
@@ -17,33 +17,6 @@ public class Box : ObjectBase
     PlayerBase player;
 
     bool isBroken = false;
-
-    void Start()
-    {
-        //SpawnBox関数を繰り返し呼び出して、箱を繰り返し生成する
-        InvokeRepeating("SpawnBox", 5.5f, 10);
-    }
-
-    /// <summary>
-    /// 箱を生成する処理
-    /// </summary>
-    public void SpawnBox()
-    {
-        GameObject boxObj = BoxPrefab;
-        float spawnX = Random.Range(1, 3);
-
-
-        if (spawnX==1)
-        {
-            //箱を生成する
-            Instantiate(boxObj, new Vector2(27.09f, 27), Quaternion.identity);
-        }
-        if (spawnX==2)
-        {
-            //箱を生成する
-            Instantiate(boxObj, new Vector2(28.9f, 27), Quaternion.identity);
-        }
-    }
 
     /// <summary>
     /// 箱を消す処理
