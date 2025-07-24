c96ceb085d4ddfcab9c1c6eb098963789161a666
Souma Ueno <231116@st.yoshida-g.ac.jp>
2025-07-24 18:48:10 +0900

    ステージ更新・レリック総入れ替えを修正

--- Client/Assets/99_User/01_Ueno/02_Script/Scene/TitleManager.cs
+++ Client/Assets/99_User/01_Ueno/02_Script/Scene/TitleManager.cs
@@ -3,6 +3,14 @@ using UnityEngine.SceneManagement;
 
 public class TitleManager : MonoBehaviour
 {
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        GameObject obj = GameObject.Find("UISet");
+
+        Destroy(obj);
+    }
+
     public void ButtonPush()
     {
         SceneManager.LoadScene("Stage");
