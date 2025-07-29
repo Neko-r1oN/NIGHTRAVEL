cb9ce09fa15858ae525a81556491615d25530d73
KosukeKida <231122@st.yoshida-g.ac.jp>
2025-07-29 19:28:58 +0900

    レリックの生成同期を仮実装

--- Client/Assets/99_User/01_Ueno/02_Script/Relic/RelicManager.cs
+++ Client/Assets/99_User/01_Ueno/02_Script/Relic/RelicManager.cs
@@ -61,6 +61,15 @@ public class RelicManager : MonoBehaviour
         }
     }
 
+    private void Start()
+    {
+        //モデルでOnSpawnedRelicが呼び出されるとOnSpawnRelicが呼び出される
+        RoomModel.Instance.OnSpawnedRelic += this.OnSpawnRelic;
+
+        //5秒間ごとにGenerateRelicTestを呼び出す
+        InvokeRepeating("GenerateRelicTest", 5.0f, 5.0f);
+    }
+
     /// <summary>
     /// レアリティの確率の設定
     /// </summary>
@@ -126,7 +135,7 @@ public class RelicManager : MonoBehaviour
     }
 
         [ContextMenu("GenerateRelicTest")]
-    public void GenerateRelicTest()
+    public async void GenerateRelicTest()
     {
         randomRarity = GetRandomRarity();
 
@@ -141,7 +150,11 @@ public class RelicManager : MonoBehaviour
         {
             int random = Random.Range(0, filteredRelics.Count);
             GameObject selectedRelic = filteredRelics[random];
-            relic = Instantiate(selectedRelic, new Vector3(0, 0, 0), Quaternion.identity);
+            //relic = Instantiate(selectedRelic, new Vector3(0, 0, 0), Quaternion.identity);
+            //レリックの設定
+            relic = selectedRelic;
+            //レリックの生成同期を実行
+            await RoomModel.Instance.SpawnRelicAsync(relic.transform.position);
         }
 
         if (relic != null)
@@ -152,6 +165,16 @@ public class RelicManager : MonoBehaviour
             Vector3 force = new Vector3(boundRnd, 12.0f, 0f);    // 力を設定
             rb.AddForce(force, ForceMode2D.Impulse);             // 力を加える
         }
+    }
+
+    /// <summary>
+    /// レリックの生成の通知
+    /// </summary>
+    /// <param name="relicId"></param>
+    /// <param name="pos"></param>
+    void OnSpawnRelic(int relicId,Vector2 pos)
+    {
+       relic = Instantiate(relicPrefab[relicId], pos,Quaternion.identity);
     }
 
     [ContextMenu("ShuffleRelic")]
