359524d9629072496599bd9df5789fbb8bd7084d
Rui Enomoto <231013@st.yoshida-g.ac.jp>
2025-08-05 10:58:07 +0900

    プレイヤー参照部分のエラー修正

--- Client/Assets/99_User/05_Miura/02_Script/BackGround.cs
+++ Client/Assets/99_User/05_Miura/02_Script/BackGround.cs
@@ -2,21 +2,17 @@ using UnityEngine;
 
 public class BackGround : MonoBehaviour
 {
-    PlayerBase player;
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        player=CharacterManager.Instance.PlayerObjSelf.GetComponent<PlayerBase>();
-    }
-
     // Update is called once per frame
     void Update()
     {
-        //BackCity‚Ì“®‚«
-        this.transform.GetChild(0).position=new Vector2(-(player.transform.position.x/7), 3.25f);
+        if (Camera.main.gameObject.GetComponent<CameraFollow>().Target)
+        {
+            GameObject player = Camera.main.gameObject.GetComponent<CameraFollow>().Target.gameObject;
+            //BackCity‚Ì“®‚«
+            this.transform.GetChild(0).position = new Vector2(-(player.transform.position.x / 7), 3.25f);
 
-        //FlontCity‚Ì“®‚«
-        this.transform.GetChild(1).position = new Vector2(-(player.transform.position.x / 4), -1.6f);
+            //FlontCity‚Ì“®‚«
+            this.transform.GetChild(1).position = new Vector2(-(player.transform.position.x / 4), -1.6f);
+        }
     }
 }
