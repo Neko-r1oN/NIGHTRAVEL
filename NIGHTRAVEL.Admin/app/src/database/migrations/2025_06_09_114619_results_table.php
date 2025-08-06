d15eefdbfeb049288e3d7ecfb9d36e3c480e9b46
KosukeKida <231122@st.yoshida-g.ac.jp>
2025-08-06 16:31:47 +0900

    レア度テーブルを追加。ステータス強化、レリックテーブルの本実装

--- NIGHTRAVEL.Admin/app/src/database/migrations/2025_06_09_114619_results_table.php
+++ NIGHTRAVEL.Admin/app/src/database/migrations/2025_06_09_114619_results_table.php
@@ -14,26 +14,17 @@ return new class extends Migration {
         Schema::create('results', function (Blueprint $table) {
             $table->id();                                        //idカラム
             $table->integer('user_id');             //user_idカラム
-            $table->integer('title_id');             //title_idカラム
-            $table->integer('weapon_id');             //weapon_idカラム
-            $table->integer('stage_id');             //user_idカラム
+            $table->integer('Character_id');             //Character_idカラム
             $table->integer('difficulty_id');             //difficulty_idカラム
-            $table->boolean('is_game_clear');             //is_game_clearカラム
-            $table->integer('total_score');             //total_scoreカラム
-            $table->integer('total_kill');             //total_killカラム
+            $table->integer('relic_count');             //relic_countカラム
+            $table->integer('stage_complete');             //stage_completeカラム
             $table->integer('character_level');             //character_levelカラム
             $table->time('alive_time');             //alive_timeカラム
-            $table->integer('max_given_damage');             //max_given_damageカラム
+            $table->integer('total_kill');             //total_killカラム
             $table->integer('given_damage');             //given_damageカラム
             $table->integer('received_damage');             //received_damageカラム
-            $table->integer('stage_exit_count');             //stage_exit_countカラム
-            $table->integer('relic_count');             //relic_countカラム
-            $table->integer('power_up_count');             //power_up_countカラム
-            $table->float('move_distance');             //move_distanceカラム
-            $table->integer('boss_kill_count');             //boss_kill_countカラム
-            $table->integer('stage_complete');             //stage_completeカラム
-            $table->time('play_time');             //play_timeカラム
-            $table->integer('dead_count');             //dead_countカラム
+            $table->integer('total_get_item');             //total_get_itemカラム
+            $table->integer('total_launch_device');             //total_launch_deviceカラム
             $table->timestamps();                               //created_atとupdated_at
 
             $table->unique('id');                    //idにユニーク制約設定
--- NIGHTRAVEL.Admin/app/src/database/seeders/RelicsTableSeeder.php
+++ NIGHTRAVEL.Admin/app/src/database/seeders/RelicsTableSeeder.php
@@ -14,22 +14,130 @@ class RelicsTableSeeder extends Seeder
     public function run(): void
     {
         Relic::create([
-            'name' => 'テストレリック1',
-            'effect' => 1.5,
-            'explanation' => 'ここにレリック1の効果説明文が入ります',
+            'name' => 'アタックチップ',
+            'effect' => 1.05,
+            'explanation' => '攻撃力上昇+5%',
             'rarity' => 1,
         ]);
         Relic::create([
-            'name' => 'テストレリック2',
-            'effect' => 3.0,
-            'explanation' => 'ここにレリック2の効果説明文',
+            'name' => 'ディフェンスチップ',
+            'effect' => 1.03,
+            'explanation' => '防御力上昇+3%',
+            'rarity' => 1,
+        ]);
+        Relic::create([
+            'name' => 'ムーブスピードスチップ',
+            'effect' => 2.0,
+            'explanation' => '移動速度+2',
+            'rarity' => 1,
+        ]);
+        Relic::create([
+            'name' => 'アタックスピードスチップ',
+            'effect' => 0.02,
+            'explanation' => '攻撃速度+0.05',
+            'rarity' => 1,
+        ]);
+        Relic::create([
+            'name' => '冷却ファン',
+            'effect' => 0.15,
+            'explanation' => '攻撃に凍結効果を付与　15%で発動',
+            'rarity' => 1,
+        ]);
+        Relic::create([
+            'name' => '加熱ファン',
+            'effect' => 0.15,
+            'explanation' => '攻撃に炎上効果を付与　15%で発動',
+            'rarity' => 1,
+        ]);
+        Relic::create([
+            'name' => '液漏れ電池',
+            'effect' => 0.15,
+            'explanation' => '攻撃に感電効果を付与　15%で発動',
+            'rarity' => 1,
+        ]);
+        Relic::create([
+            'name' => 'ビットコイン',
+            'effect' => 1.30,
+            'explanation' => '経験値獲得量が30%増加',
+            'rarity' => 1,
+        ]);
+        Relic::create([
+            'name' => 'リゲインコード',
+            'effect' => 1.02,
+            'explanation' => '与えたダメージの10%分回復する',
+            'rarity' => 1,
+        ]);
+        Relic::create([
+            'name' => 'スキャターバグ',
+            'effect' => 1.05,
+            'explanation' => '攻撃時に2つボム(PLの攻撃力30%)をばらまく。',
+            'rarity' => 1,
+        ]);
+        Relic::create([
+            'name' => 'ホログラムアーマー',
+            'effect' => 1.15,
+            'explanation' => '攻撃を15%の確率で回避する',
+            'rarity' => 2,
+        ]);
+        Relic::create([
+            'name' => 'マウス',
+            'effect' => 1.10,
+            'explanation' => '20%の確率でスキルのクールダウンをリセット',
+            'rarity' => 2,
+        ]);
+        Relic::create([
+            'name' => 'デジタルミート',
+            'effect' => 3,
+            'explanation' => '20秒ごとに最大HP5%を回復する肉塊を3つ近くに生成する　15秒経過で消滅する',
+            'rarity' => 2,
+        ]);
+        Relic::create([
+            'name' => 'ファイアウォール',
+            'effect' => 1.20,
+            'explanation' => '被ダメージを20%軽減',
+            'rarity' => 2,
+        ]);
+        Relic::create([
+            'name' => 'B.A.Nハンマー',
+            'effect' => 1.025,
+            'explanation' => '経験値獲得量が30%増加',
+            'rarity' => 2,
+        ]);
+        Relic::create([
+            'name' => 'ライフスカベンジャー',
+            'effect' => 1.005,
+            'explanation' => '敵撃破時にHPを1%分回復する',
+            'rarity' => 2,
+        ]);
+        Relic::create([
+            'name' => 'ラグルーター',
+            'effect' => 1.10,
+            'explanation' => '攻撃時に2回ヒットする。(2回目の攻撃はダメージ50%)',
             'rarity' => 3,
         ]);
         Relic::create([
-            'name' => 'テストレリック3',
-            'effect' => 2.0,
-            'explanation' => 'ここにレリック3の効果説明文',
-            'rarity' => 1,
+            'name' => 'バックアップHDMI',
+            'effect' => 1,
+            'explanation' => '一度復活する　その後破壊される',
+            'rarity' => 3,
+        ]);
+        Relic::create([
+            'name' => '識別AI',
+            'effect' => 1,
+            'explanation' => '状態異常が付与されている敵に対して与える',
+            'rarity' => 3,
+        ]);
+        Relic::create([
+            'name' => '帯電コア',
+            'effect' => 8,
+            'explanation' => '攻撃ヒット時、基礎攻撃力の800%のダメージを与える広範囲の電磁波を放つ　クールダウン10秒',
+            'rarity' => 4,
+        ]);
+        Relic::create([
+            'name' => 'Caracal.png',
+            'effect' => 1.10,
+            'explanation' => '効果なし うざい',
+            'rarity' => 5,
         ]);
     }
 }
