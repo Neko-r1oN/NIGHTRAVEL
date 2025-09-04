73b8e2ab848a29f7f83eb9d2ce8f457aaee9fff1
KosukeKida <231122@st.yoshida-g.ac.jp>
2025-09-04 12:29:02 +0900

    レリックのデータベース実装。DBコンテキストと同じに調整

--- NIGHTRAVEL.Admin/app/src/database/seeders/RelicsTableSeeder.php
+++ NIGHTRAVEL.Admin/app/src/database/seeders/RelicsTableSeeder.php
@@ -19,7 +19,7 @@ class RelicsTableSeeder extends Seeder
             'rate_effect' => 1.05,
             'calculation_method' => 1,
             'max' => 0,
-            'status_type' => 'Power',
+            'status_type' => 'AttackTipRate',
             'explanation' => '攻撃力上昇+5%',
             'rarity' => 1,
         ]);
@@ -29,7 +29,7 @@ class RelicsTableSeeder extends Seeder
             'rate_effect' => 1.03,
             'calculation_method' => 1,
             'max' => 0,
-            'status_type' => 'Defense',
+            'status_type' => 'DeffenceTipRate',
             'explanation' => '防御力上昇+3%',
             'rarity' => 1,
         ]);
@@ -39,7 +39,7 @@ class RelicsTableSeeder extends Seeder
             'rate_effect' => 1,
             'calculation_method' => 1,
             'max' => 0,
-            'status_type' => 'MoveSpeed',
+            'status_type' => 'MoveSpeedRate',
             'explanation' => '移動速度+2',
             'rarity' => 1,
         ]);
@@ -49,7 +49,7 @@ class RelicsTableSeeder extends Seeder
             'rate_effect' => 1,
             'calculation_method' => 1,
             'max' => 0,
-            'status_type' => 'AttackSpeedFactor',
+            'status_type' => 'AttackSpeedRate',
             'explanation' => '攻撃速度+0.05',
             'rarity' => 1,
         ]);
@@ -59,7 +59,7 @@ class RelicsTableSeeder extends Seeder
             'rate_effect' => 1.15,
             'calculation_method' => 1,
             'max' => 1.15,
-            'status_type' => 'GiveDebuffRates',
+            'status_type' => 'CoolingRate',
             'explanation' => '攻撃に凍結効果を付与　15%で発動',
             'rarity' => 1,
         ]);
@@ -69,7 +69,7 @@ class RelicsTableSeeder extends Seeder
             'rate_effect' => 1.15,
             'calculation_method' => 1,
             'max' => 1.15,
-            'status_type' => 'GiveDebuffRates',
+            'status_type' => 'BurningRate',
             'explanation' => '攻撃に炎上効果を付与　15%で発動',
             'rarity' => 1,
         ]);
@@ -79,7 +79,7 @@ class RelicsTableSeeder extends Seeder
             'rate_effect' => 1.15,
             'calculation_method' => 1,
             'max' => 1.15,
-            'status_type' => 'GiveDebuffRates',
+            'status_type' => 'ShokingRate',
             'explanation' => '攻撃に感電効果を付与　15%で発動',
             'rarity' => 1,
         ]);
@@ -89,7 +89,7 @@ class RelicsTableSeeder extends Seeder
             'rate_effect' => 1.15,
             'calculation_method' => 2,
             'max' => 0,
-            'status_type' => 'PierceRate',
+            'status_type' => 'BitCoinRate',
             'explanation' => '経験値獲得量が30%増加',
             'rarity' => 1,
         ]);
@@ -99,7 +99,7 @@ class RelicsTableSeeder extends Seeder
             'rate_effect' => 1.02,
             'calculation_method' => 1,
             'max' => 0,
-            'status_type' => 'DmgHealRate',
+            'status_type' => 'RegainCodeRate',
             'explanation' => '与えたダメージの10%分回復する',
             'rarity' => 1,
         ]);
@@ -109,7 +109,7 @@ class RelicsTableSeeder extends Seeder
             'rate_effect' => 1,
             'calculation_method' => 1,
             'max' => 0,
-            'status_type' => 'BombCnt',
+            'status_type' => 'ScatterBugCnt',
             'explanation' => '攻撃時に2つボム(PLの攻撃力30%)をばらまく。',
             'rarity' => 1,
         ]);
@@ -119,7 +119,7 @@ class RelicsTableSeeder extends Seeder
             'rate_effect' => 1.15,
             'calculation_method' => 2,
             'max' => 0,
-            'status_type' => 'Defense',
+            'status_type' => 'HolographicArmorRate',
             'explanation' => '攻撃を15%の確率で回避する',
             'rarity' => 2,
         ]);
@@ -129,7 +129,7 @@ class RelicsTableSeeder extends Seeder
             'rate_effect' => 1.10,
             'calculation_method' => 2,
             'max' => 0,
-            'status_type' => 'DodgeRate',
+            'status_type' => 'MouseRate',
             'explanation' => '20%の確率でスキルのクールダウンをリセット',
             'rarity' => 2,
         ]);
@@ -139,7 +139,7 @@ class RelicsTableSeeder extends Seeder
             'rate_effect' => 1,
             'calculation_method' => 1,
             'max' => 0,
-            'status_type' => 'HealMeatCnt',
+            'status_type' => 'DigitalMeatCnt',
             'explanation' => '20秒ごとに最大HP5%を回復する肉塊を3つ近くに生成する　15秒経過で消滅する',
             'rarity' => 2,
         ]);
@@ -149,7 +149,7 @@ class RelicsTableSeeder extends Seeder
             'rate_effect' => 1.20,
             'calculation_method' => 2,
             'max' => 0,
-            'status_type' => 'DmgResistRate',
+            'status_type' => 'FirewallRate',
             'explanation' => '被ダメージを20%軽減',
             'rarity' => 2,
         ]);
@@ -159,7 +159,7 @@ class RelicsTableSeeder extends Seeder
             'rate_effect' => 1.002,
             'calculation_method' => 1,
             'max' => 1.03,
-            'status_type' => 'KillHpReward',
+            'status_type' => 'LifeScavengerRate',
             'explanation' => '敵撃破時にHPを1%分回復する',
             'rarity' => 2,
         ]);
@@ -169,7 +169,7 @@ class RelicsTableSeeder extends Seeder
             'rate_effect' => 1.10,
             'calculation_method' => 2,
             'max' => 1.0,
-            'status_type' => 'DARate',
+            'status_type' => 'RugrouterRate',
             'explanation' => '攻撃時に2回ヒットする。(2回目の攻撃はダメージ50%)',
             'rarity' => 3,
         ]);
@@ -179,7 +179,7 @@ class RelicsTableSeeder extends Seeder
             'rate_effect' => 0,
             'calculation_method' => 1,
             'max' => 1.0,
-            'status_type' => 'HP',
+            'status_type' => 'BuckupHDMICnt',
             'explanation' => '一度復活する　その後破壊される',
             'rarity' => 3,
         ]);
@@ -189,7 +189,7 @@ class RelicsTableSeeder extends Seeder
             'rate_effect' => 1.0,
             'calculation_method' => 1,
             'max' => 0,
-            'status_type' => 'All',
+            'status_type' => 'IdentificationAIRate',
             'explanation' => '状態異常が付与されている敵に対して与える',
             'rarity' => 3,
         ]);
@@ -199,7 +199,7 @@ class RelicsTableSeeder extends Seeder
             'rate_effect' => 1.05,
             'calculation_method' => 1,
             'max' => 1.50,
-            'status_type' => 'Power',
+            'status_type' => 'DanborDollRate',
             'explanation' => '相手の防御を10%無視する。',
             'rarity' => 4,
         ]);
@@ -209,7 +209,7 @@ class RelicsTableSeeder extends Seeder
             'rate_effect' => 0,
             'calculation_method' => 1,
             'max' => 6,
-            'status_type' => 'ElecOrbCnt',
+            'status_type' => 'ChargedCoreCnt',
             'explanation' => '自身を中心に回転する電気玉を発生する(攻撃力15%)',
             'rarity' => 4,
         ]);
@@ -219,7 +219,7 @@ class RelicsTableSeeder extends Seeder
             'rate_effect' => 1.01,
             'calculation_method' => 1,
             'max' => 0,
-            'status_type' => 'Power',
+            'status_type' => 'IllegalScriptRate',
             'explanation' => 'ボスを除いて3%の確率で99999ダメージを与える',
             'rarity' => 4,
         ]);
@@ -229,7 +229,7 @@ class RelicsTableSeeder extends Seeder
             'rate_effect' => 1,
             'calculation_method' => 1,
             'max' => 0,
-            'status_type' => 'All',
+            'status_type' => 'CaracalPngRate',
             'explanation' => '効果なし うざい',
             'rarity' => 5,
         ]);
