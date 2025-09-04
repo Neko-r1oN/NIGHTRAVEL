eddbbdc93b305c55721435c22f435f54aaf607e4
KosukeKida <231122@st.yoshida-g.ac.jp>
2025-09-04 10:44:41 +0900

    データベース入力。残り、データ分の内容のみ

--- NIGHTRAVEL.Admin/app/src/database/seeders/RelicsTableSeeder.php
+++ NIGHTRAVEL.Admin/app/src/database/seeders/RelicsTableSeeder.php
@@ -65,92 +65,162 @@ class RelicsTableSeeder extends Seeder
         ]);
         Relic::create([
             'name' => '加熱ファン',
-            'effect' => 0.15,
+            'const_effect' => 0,
+            'rate_effect' => 1.15,
+            'calculation_method' => 1,
+            'max' => 0.15,
+            'status_type' => 'GiveDebuffRates',
             'explanation' => '攻撃に炎上効果を付与　15%で発動',
             'rarity' => 1,
         ]);
         Relic::create([
             'name' => '液漏れ電池',
-            'effect' => 0.15,
+            'const_effect' => 0,
+            'rate_effect' => 1.15,
+            'calculation_method' => 1,
+            'max' => 0.15,
+            'status_type' => 'GiveDebuffRates',
             'explanation' => '攻撃に感電効果を付与　15%で発動',
             'rarity' => 1,
         ]);
         Relic::create([
             'name' => 'ビットコイン',
-            'effect' => 1.30,
+            'const_effect' => 0,
+            'rate_effect' => 1.15,
+            'calculation_method' => 2,
+            'max' => 0,
+            'status_type' => 'PierceRate',
             'explanation' => '経験値獲得量が30%増加',
             'rarity' => 1,
         ]);
         Relic::create([
             'name' => 'リゲインコード',
-            'effect' => 1.02,
+            'const_effect' => 0,
+            'rate_effect' => 1.02,
+            'calculation_method' => 1,
+            'max' => 0,
+            'status_type' => 'DmgHealRate',
             'explanation' => '与えたダメージの10%分回復する',
             'rarity' => 1,
         ]);
         Relic::create([
             'name' => 'スキャターバグ',
-            'effect' => 1.05,
+            'const_effect' => 1,
+            'rate_effect' => 1,
+            'calculation_method' => 1,
+            'max' => 0,
+            'status_type' => 'BombCnt',
             'explanation' => '攻撃時に2つボム(PLの攻撃力30%)をばらまく。',
             'rarity' => 1,
         ]);
         Relic::create([
             'name' => 'ホログラムアーマー',
-            'effect' => 1.15,
+            'const_effect' => 0,
+            'rate_effect' => 1.15,
+            'calculation_method' => 2,
+            'max' => 0,
+            'status_type' => 'DARate',
             'explanation' => '攻撃を15%の確率で回避する',
             'rarity' => 2,
         ]);
         Relic::create([
             'name' => 'マウス',
-            'effect' => 1.10,
+            'const_effect' => 0,
+            'rate_effect' => 1.10,
+            'calculation_method' => 2,
+            'max' => 0,
+            'status_type' => 'DodgeRate',
             'explanation' => '20%の確率でスキルのクールダウンをリセット',
             'rarity' => 2,
         ]);
         Relic::create([
             'name' => 'デジタルミート',
-            'effect' => 3,
+            'const_effect' => 1,
+            'rate_effect' => 1,
+            'calculation_method' => 1,
+            'max' => 0,
+            'status_type' => 'HealMeatCnt',
             'explanation' => '20秒ごとに最大HP5%を回復する肉塊を3つ近くに生成する　15秒経過で消滅する',
             'rarity' => 2,
         ]);
         Relic::create([
             'name' => 'ファイアウォール',
-            'effect' => 1.20,
+            'const_effect' => 1,
+            'rate_effect' => 1.20,
+            'calculation_method' => 2,
+            'max' => 0,
+            'status_type' => 'DmgResistRate',
             'explanation' => '被ダメージを20%軽減',
             'rarity' => 2,
         ]);
         Relic::create([
-            'name' => 'B.A.Nハンマー',
-            'effect' => 1.025,
-            'explanation' => '経験値獲得量が30%増加',
-            'rarity' => 2,
-        ]);
-        Relic::create([
             'name' => 'ライフスカベンジャー',
-            'effect' => 1.005,
+            'const_effect' => 0,
+            'rate_effect' => 1.002,
+            'calculation_method' => 1,
+            'max' => 1.03,
+            'status_type' => 'KillHpReward',
             'explanation' => '敵撃破時にHPを1%分回復する',
             'rarity' => 2,
         ]);
         Relic::create([
             'name' => 'ラグルーター',
-            'effect' => 1.10,
+            'const_effect' => 0,
+            'rate_effect' => 1.10,
+            'calculation_method' => 2,
+            'max' => 1.0,
+            'status_type' => 'DARate',
             'explanation' => '攻撃時に2回ヒットする。(2回目の攻撃はダメージ50%)',
             'rarity' => 3,
         ]);
         Relic::create([
             'name' => 'バックアップHDMI',
-            'effect' => 1,
+            'const_effect' => 1,
+            'rate_effect' => 0,
+            'calculation_method' => 1,
+            'max' => 1.0,
+            'status_type' => 'All',
             'explanation' => '一度復活する　その後破壊される',
             'rarity' => 3,
         ]);
         Relic::create([
             'name' => '識別AI',
-            'effect' => 1,
+            'const_effect' => 0,
+            'rate_effect' => 1.0,
+            'calculation_method' => 1,
+            'max' => 0,
+            'status_type' => 'All',
             'explanation' => '状態異常が付与されている敵に対して与える',
             'rarity' => 3,
         ]);
         Relic::create([
+            'name' => '段ボール人形',
+            'const_effect' => 0,
+            'rate_effect' => 1.0,
+            'calculation_method' => 1,
+            'max' => 0,
+            'status_type' => 'All',
+            'explanation' => '相手の防御を10%無視する。',
+            'rarity' => 4,
+        ]);
+        Relic::create([
             'name' => '帯電コア',
-            'effect' => 8,
+            'const_effect' => 0,
+            'rate_effect' => 1.0,
+            'calculation_method' => 1,
+            'max' => 0,
+            'status_type' => 'All',
             'explanation' => '攻撃ヒット時、基礎攻撃力の800%のダメージを与える広範囲の電磁波を放つ　クールダウン10秒',
+            'rarity' => 4,
+        ]);
+        Relic::create([
+            'name' => '違法スクリプト',
+            'const_effect' => 0,
+            'rate_effect' => 1.0,
+            'calculation_method' => 1,
+            'max' => 0,
+            'status_type' => 'All',
+            'explanation' => 'ボスを除いて3%の確率で99999ダメージを与える',
             'rarity' => 4,
         ]);
         Relic::create([
