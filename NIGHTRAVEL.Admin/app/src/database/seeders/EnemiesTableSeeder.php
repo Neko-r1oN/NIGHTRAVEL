ba03c612ed6859a9a96b297d53f36e5074a0e853
KosukeKida <231122@st.yoshida-g.ac.jp>
2025-09-08 09:28:59 +0900

    敵のデータベース修正。

--- NIGHTRAVEL.Admin/app/src/database/seeders/EnemiesTableSeeder.php
+++ NIGHTRAVEL.Admin/app/src/database/seeders/EnemiesTableSeeder.php
@@ -14,30 +14,244 @@ class EnemiesTableSeeder extends Seeder
     public function run(): void
     {
         Enemy::create([
-            'name' => 'ドローン',
-            'stage_id' => '1,2,4',
-            'hp' => 10.0,
-            'attack' => 5.0,
-            'defence' => 1.0,
-            'move_speed' => 20.0
+            'name' => 'Hakonov_TypeA',
+            'isboss' => false,
+            'hp' => 100,
+            'defence' => 15,
+            'power' => 35,
+            'jump_power' => 7.0,
+            'move_speed' => 8.0,
+            'attack_speed_factor' => 1.0,
+            'stage_id' => '1',
+            'exp' => 5
         ]);
-
         Enemy::create([
-            'name' => 'ロボ警備員',
-            'stage_id' => '1,2',
-            'hp' => 20.0,
-            'attack' => 8.0,
-            'defence' => 1.0,
-            'move_speed' => 10.0
+            'name' => 'Hakonov_TypeB',
+            'isboss' => false,
+            'hp' => 100,
+            'defence' => 15,
+            'power' => 35,
+            'jump_power' => 7.0,
+            'move_speed' => 8.0,
+            'attack_speed_factor' => 1.0,
+            'stage_id' => '1',
+            'exp' => 5
         ]);
-
         Enemy::create([
-            'name' => 'ロボドッグ',
-            'stage_id' => '1,3',
-            'hp' => 5.0,
-            'attack' => 8.0,
-            'defence' => 1.0,
-            'move_speed' => 30.0
+            'name' => 'Hakonov_TypeC',
+            'isboss' => false,
+            'hp' => 100,
+            'defence' => 15,
+            'power' => 35,
+            'jump_power' => 7.0,
+            'move_speed' => 8.0,
+            'attack_speed_factor' => 1.0,
+            'stage_id' => '1',
+            'exp' => 5
+        ]);
+        Enemy::create([
+            'name' => 'Signabot',
+            'isboss' => false,
+            'hp' => 100,
+            'defence' => 15,
+            'power' => 35,
+            'jump_power' => 7.0,
+            'move_speed' => 8.0,
+            'attack_speed_factor' => 1.0,
+            'stage_id' => '1',
+            'exp' => 5
+        ]);
+        Enemy::create([
+            'name' => 'Delibot',
+            'isboss' => false,
+            'hp' => 100,
+            'defence' => 15,
+            'power' => 35,
+            'jump_power' => 7.0,
+            'move_speed' => 8.0,
+            'attack_speed_factor' => 1.0,
+            'stage_id' => '1',
+            'exp' => 5
+        ]);
+        Enemy::create([
+            'name' => 'Vendbot',
+            'isboss' => false,
+            'hp' => 100,
+            'defence' => 15,
+            'power' => 35,
+            'jump_power' => 7.0,
+            'move_speed' => 8.0,
+            'attack_speed_factor' => 1.0,
+            'stage_id' => '1',
+            'exp' => 5
+        ]);
+        Enemy::create([
+            'name' => 'Boxgeist',
+            'isboss' => false,
+            'hp' => 100,
+            'defence' => 15,
+            'power' => 35,
+            'jump_power' => 7.0,
+            'move_speed' => 8.0,
+            'attack_speed_factor' => 1.0,
+            'stage_id' => '1',
+            'exp' => 5
+        ]);
+        Enemy::create([
+            'name' => 'Drone',
+            'isboss' => false,
+            'hp' => 100,
+            'defence' => 15,
+            'power' => 35,
+            'jump_power' => 7.0,
+            'move_speed' => 8.0,
+            'attack_speed_factor' => 1.0,
+            'stage_id' => '1',
+            'exp' => 5
+        ]);
+        Enemy::create([
+            'name' => 'Guardbot',
+            'isboss' => false,
+            'hp' => 100,
+            'defence' => 15,
+            'power' => 35,
+            'jump_power' => 7.0,
+            'move_speed' => 8.0,
+            'attack_speed_factor' => 1.0,
+            'stage_id' => '1',
+            'exp' => 5
+        ]);
+        Enemy::create([
+            'name' => 'CyberDog',
+            'isboss' => false,
+            'hp' => 100,
+            'defence' => 15,
+            'power' => 35,
+            'jump_power' => 7.0,
+            'move_speed' => 8.0,
+            'attack_speed_factor' => 1.0,
+            'stage_id' => '2',
+            'exp' => 5
+        ]);
+        Enemy::create([
+            'name' => 'MissileMachine',
+            'isboss' => false,
+            'hp' => 100,
+            'defence' => 15,
+            'power' => 35,
+            'jump_power' => 7.0,
+            'move_speed' => 8.0,
+            'attack_speed_factor' => 1.0,
+            'stage_id' => '2',
+            'exp' => 5
+        ]);
+        Enemy::create([
+            'name' => 'CyberDog_ByWorm',
+            'isboss' => false,
+            'hp' => 100,
+            'defence' => 15,
+            'power' => 35,
+            'jump_power' => 7.0,
+            'move_speed' => 8.0,
+            'attack_speed_factor' => 1.0,
+            'stage_id' => '2',
+            'exp' => 5
+        ]);
+        Enemy::create([
+            'name' => 'Drone_ByWorm',
+            'isboss' => false,
+            'hp' => 100,
+            'defence' => 15,
+            'power' => 35,
+            'jump_power' => 7.0,
+            'move_speed' => 8.0,
+            'attack_speed_factor' => 1.0,
+            'stage_id' => '2',
+            'exp' => 5
+        ]);
+        Enemy::create([
+            'name' => 'FullMetalWorm',
+            'isboss' => false,
+            'hp' => 100,
+            'defence' => 15,
+            'power' => 35,
+            'jump_power' => 7.0,
+            'move_speed' => 8.0,
+            'attack_speed_factor' => 1.0,
+            'stage_id' => '2',
+            'exp' => 5
+        ]);
+        Enemy::create([
+            'name' => 'MetalBody',
+            'isboss' => false,
+            'hp' => 100,
+            'defence' => 15,
+            'power' => 35,
+            'jump_power' => 7.0,
+            'move_speed' => 8.0,
+            'attack_speed_factor' => 1.0,
+            'stage_id' => '2',
+            'exp' => 5
+        ]);
+        Enemy::create([
+            'name' => 'Carcass',
+            'isboss' => false,
+            'hp' => 100,
+            'defence' => 15,
+            'power' => 35,
+            'jump_power' => 7.0,
+            'move_speed' => 8.0,
+            'attack_speed_factor' => 1.0,
+            'stage_id' => '3',
+            'exp' => 5
+        ]);
+        Enemy::create([
+            'name' => 'Node_Core',
+            'isboss' => false,
+            'hp' => 100,
+            'defence' => 15,
+            'power' => 35,
+            'jump_power' => 7.0,
+            'move_speed' => 8.0,
+            'attack_speed_factor' => 1.0,
+            'stage_id' => '3',
+            'exp' => 5
+        ]);
+        Enemy::create([
+            'name' => 'Slade',
+            'isboss' => false,
+            'hp' => 100,
+            'defence' => 15,
+            'power' => 35,
+            'jump_power' => 7.0,
+            'move_speed' => 8.0,
+            'attack_speed_factor' => 1.0,
+            'stage_id' => '3',
+            'exp' => 5
+        ]);
+        Enemy::create([
+            'name' => 'Blaze',
+            'isboss' => false,
+            'hp' => 100,
+            'defence' => 15,
+            'power' => 35,
+            'jump_power' => 7.0,
+            'move_speed' => 8.0,
+            'attack_speed_factor' => 1.0,
+            'stage_id' => '3',
+            'exp' => 5
+        ]);
+        Enemy::create([
+            'name' => 'Valcus',
+            'isboss' => false,
+            'hp' => 100,
+            'defence' => 15,
+            'power' => 35,
+            'jump_power' => 7.0,
+            'move_speed' => 8.0,
+            'attack_speed_factor' => 1.0,
+            'stage_id' => '3',
+            'exp' => 5
         ]);
     }
 }
