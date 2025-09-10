c706724ef33fc01ce9468e70fc87fa739d41bb6a
Rui Enomoto <231013@st.yoshida-g.ac.jp>
2025-09-10 13:21:36 +0900

    ノード・コア実装

---
+++ Client/Assets/99_User/02_Enomoto/02_Script/Wepon/NodeBullet.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using static CharacterBase;
+using static Shared.Interfaces.StreamingHubs.EnumManager;
+
+public class NodeBullet : ProjectileBase
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // レイヤーが地形やギミックの場合
+        if (collision.gameObject.layer == 0 || collision.gameObject.layer == 13)
+        {
+            this.Destroy();
+        }
+        else if (collision.gameObject.tag == "Player")
+        {
+            DEBUFF_TYPE? debuff = null;
+            if (debuffs.Count > 0) debuff = debuffs[0];
+            collision.gameObject.GetComponent<PlayerBase>().ApplyDamage(power, null, KB_POW.Medium, debuff);
+        }
+    }
+
+    protected override void Destroy()
+    {
+        // 着弾パーティクル生成
+
+
+        Destroy(this.gameObject);
+    }
+}
