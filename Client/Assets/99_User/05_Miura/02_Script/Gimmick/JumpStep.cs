28dc56de4128ca46d7d36aec25b3584410d857ee
Miura\student <231049@st.yoshida-g.ac.jp>
2025-09-25 22:50:01 +0900

    GimmickScene、ステージのシーンを編集。ジャンプ台に効果音を追加。ジャンプ台のプレハブを編集。

--- Client/Assets/99_User/05_Miura/02_Script/Gimmick/JumpStep.cs
+++ Client/Assets/99_User/05_Miura/02_Script/Gimmick/JumpStep.cs
@@ -10,6 +10,9 @@ public class JumpStep : MonoBehaviour
     //加える力の量の変数
     public float addPow;
 
+    //ジャンプ台の効果音
+    [SerializeField] AudioSource jumpStepSE;
+
     /// <summary>
     /// 触れたオブジェクトに力を加える処理
     /// </summary>
@@ -20,6 +23,9 @@ public class JumpStep : MonoBehaviour
         {
             //ぶつかったオブジェクトに、addPow分の力を加える
             collision.gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, addPow));
+
+            //効果音を再生する
+            jumpStepSE.Play();
         }
     }
 }
