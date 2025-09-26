a100fb19079d5b02e9b1a550492bd393d721536b
Rui Enomoto <231013@st.yoshida-g.ac.jp>
2025-09-26 14:50:16 +0900

    電気床のバグ修正、ステージ１に設置されてあるブロックにコライダーアタッチ

--- Client/Assets/99_User/05_Miura/02_Script/Gimmick/ShortCircuit.cs
+++ Client/Assets/99_User/05_Miura/02_Script/Gimmick/ShortCircuit.cs
@@ -1,112 +1,65 @@
+using System.Collections;
 using UnityEngine;
-using UnityEngine.UIElements;
 
 public class ShortCircuit : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] 
+    AudioSource shortCircuitSE;
 
-    PlayerBase playerBase;
-    EnemyBase enemyBase;
+    [SerializeField] 
+    Transform attackCheckPoint;
 
-    [SerializeField] AudioSource shortCircuitSE;
+    [SerializeField]
+    Vector2 attackRange = Vector2.zero;
 
-    Vector2 pos = Vector2.zero;
+    [SerializeField]
+    bool canDrawRay = false;
 
-    /// <summary>
-    /// プレイヤーのダメージ量計算処理
-    /// </summary>
-    void HitPlayerDamage()
+    private void Start()
     {
-        //プレイヤーの最大HPを代入
-        int maxHP = playerBase.MaxHP;
-        int HP=playerBase.HP;
-
-        //ダメージ量は最大HPの5%
-        int damage = Mathf.FloorToInt(maxHP * 0.05f);
-
-        //damageが0より小さいか0だったら
-        if(damage <= 0)
-        {//damegeを1にする
-            damage = 1;
-            playerBase.ApplyDamage(damage);
-        }
-
-        //もしプレイヤーのHPが0を下回ったか0だったら
-        if(HP<=0)
-        {
-            shortCircuitSE.Stop();
-        }
-
-        //PlayerBaseのApplyDamgeを呼び出す
-        playerBase.ApplyDamage(damage);
+        StartCoroutine(ApplyDamageCoroutine());
     }
 
     /// <summary>
-    /// 敵のダメージ量計算処理
+    /// ダメージ適用コルーチン
     /// </summary>
-    void HitEnemyDamage()
+    /// <returns></returns>
+    IEnumerator ApplyDamageCoroutine()
     {
-        //敵の最大HPを代入
-        int maxLife = enemyBase.MaxHP;
-        int HP=enemyBase.HP;
-
-        //ダメージ量は最大HPの5%
-        int damage = Mathf.FloorToInt(maxLife * 0.05f);
-
-        //damageが0より小さいか0だったら
-        if (damage <= 0)
-        {//damegeを1にする
-            damage = 1;
-            enemyBase.ApplyDamageRequest(damage);
-        }
-
-        //もし敵のHPが0を下回ったか0だったら
-        if (HP <= 0)
+        const float waitSec = 0.5f;
+        while (true)
         {
-            shortCircuitSE.Stop();
-        }
+            bool isHit = false;
 
-        //EnemyBaseのApplyDamageを呼び出す
-        enemyBase.ApplyDamageRequest(damage);
-    }
+            // 範囲内に攻撃可能なキャラクターがいるかチェック
+            Collider2D[] collidersEnemies = Physics2D.OverlapBoxAll(attackCheckPoint.transform.position, attackRange, 0);
 
-    /// <summary>
-    /// 漏電フィールドでダメージを与える処理
-    /// </summary>
-    /// <param name="collision">触れたオブジェクト</param>
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.gameObject.CompareTag("Player") && collision.gameObject == CharacterManager.Instance.PlayerObjSelf)
-        {//「Player」タグが付いたオブジェクトが触れたら
-            playerBase = collision.gameObject.GetComponent<PlayerBase>();
+            for (int i = 0; i < collidersEnemies.Length; i++)
+            {
+                var character = collidersEnemies[i].gameObject;
 
-            InvokeRepeating("HitPlayerDamage", 0.1f,0.5f);
-            shortCircuitSE.Play();
-        }
-        if (collision.gameObject.CompareTag("Enemy") && !RoomModel.Instance || RoomModel.Instance && RoomModel.Instance.IsMaster)
-        {//「Enemy」タグが付いたオブジェクトが触れたら
-            enemyBase = collision.gameObject.GetComponent<EnemyBase>();
+                if (character.tag == "Player" || character.tag == "Enemy")
+                {
+                    var damage = Mathf.FloorToInt(character.GetComponent<CharacterBase>().MaxHP * 0.05f);
 
-            InvokeRepeating("HitEnemyDamage", 0.1f, 0.5f);
-            shortCircuitSE.Play();
+                    if(character.tag == "Player") character.GetComponent<PlayerBase>().ApplyDamage(damage);
+                    else if (character.tag == "Enemy") character.GetComponent<EnemyBase>().ApplyDamageRequest(damage);
+
+                    isHit = true;
+                }
+            }
+
+            if(isHit) shortCircuitSE.Play();
+            yield return new WaitForSeconds(waitSec);
         }
     }
 
-    /// <summary>
-    /// 漏電フィールドを離れた時の処理
-    /// </summary>
-    /// <param name="collision">触れたオブジェクト</param>
-    private void OnTriggerExit2D(Collider2D collision)
+    private void OnDrawGizmos()
     {
-        if (collision.gameObject.CompareTag("Player"))
+        if (canDrawRay)
         {
-            CancelInvoke();
-            shortCircuitSE.Stop();
-        }
-        if(collision.gameObject.CompareTag("Enemy"))
-        {
-            CancelInvoke();
-            shortCircuitSE.Stop();
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireCube(attackCheckPoint.transform.position, attackRange);
         }
     }
 }
