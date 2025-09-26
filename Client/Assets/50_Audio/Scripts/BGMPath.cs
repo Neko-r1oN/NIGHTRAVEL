b2729f6f752fe5472b1a77f46ec483d299f5223c
Souma Ueno <231116@st.yoshida-g.ac.jp>
2025-09-26 13:54:41 +0900

    BGM追加

--- Client/Assets/50_Audio/Scripts/BGMPath.cs
+++ Client/Assets/50_Audio/Scripts/BGMPath.cs
@@ -8,6 +8,7 @@ public static class BGMPath{
 	public const string BOSS1  = "BGM/03_Boss/Boss1";
 	public const string BOSS2  = "BGM/03_Boss/Boss2";
 	public const string BOSS3  = "BGM/03_Boss/Boss3";
+	public const string STAGE1 = "BGM/02_Stage/Stage1";
 	public const string STAGE2 = "BGM/02_Stage/Stage2";
 	public const string STAGE3 = "BGM/02_Stage/Stage3";
 	public const string TITLE  = "BGM/01_Title/Title";
