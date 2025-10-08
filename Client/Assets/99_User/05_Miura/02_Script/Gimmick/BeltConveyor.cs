30e4fb08d309883ec85d16f52f811ebd68970d86
Nishiura <231075@st.yoshida-g.ac.jp>
2025-10-08 11:53:00 +0900

    ジップラインの火花が残留する不具合を修正 ベルトコンベアーの仕様を変更

--- Client/Assets/99_User/05_Miura/02_Script/Gimmick/BeltConveyor.cs
+++ Client/Assets/99_User/05_Miura/02_Script/Gimmick/BeltConveyor.cs
@@ -5,27 +5,21 @@ public class BeltConveyor : MonoBehaviour
     //加える力の量の変数
     public float addPow;
     public float addPowEnemy;
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     private void OnTriggerStay2D(Collider2D collision)
     {
-        if(collision.CompareTag("Player") && collision.gameObject == CharacterManager.Instance.PlayerObjSelf||collision.CompareTag("Object"))
+        // 対象がプレイヤーの場合
+        if(collision.CompareTag("Player") && collision.gameObject == CharacterManager.Instance.PlayerObjSelf)
         {
             //ぶつかったオブジェクトに、addPow分の力を加える
             collision.gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(addPow, 0));
         }
+        // 対象が敵の場合
         if(collision.CompareTag("Enemy"))
+        {
+            collision.gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(addPowEnemy, 0));
+        }
+
+        if (collision.CompareTag("Object"))
         {
             collision.gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(addPowEnemy, 0));
         }
