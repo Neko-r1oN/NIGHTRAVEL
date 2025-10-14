bdf003bf47cad83126fc4fb8b4bedb85dde1478f
Miura\student <231049@st.yoshida-g.ac.jp>
2025-10-14 14:04:42 +0900

    GimmickScene、ステージ2シーンを編集。Boxスクリプトを編集。箱のプレハブを編集

--- Client/Assets/99_User/05_Miura/02_Script/Gimmick/Box/Box.cs
+++ Client/Assets/99_User/05_Miura/02_Script/Gimmick/Box/Box.cs
@@ -7,11 +7,14 @@
 using DG.Tweening;
 using System.Threading.Tasks;
 using UnityEngine;
+using static Rewired.Glyphs.GlyphSet;
 
 public class Box : ObjectBase
 {
     [SerializeField] GameObject BoxPrefab;  //箱プレハブ取得
     [SerializeField] GameObject BoxFragment;　//破片エフェクトを取得
+    PlayerBase playerBase;
+    EnemyBase enemyBase;
 
     // 破壊判定
     bool isBroken = false;
@@ -24,41 +27,44 @@ public class Box : ObjectBase
     {
         //触れたもののタグが「Abyss」だったら
         if(collision.CompareTag("Gimmick/Abyss"))
-        {
+        {//ステージ外に出たら
             //箱を消す
             Destroy(this.gameObject);
+        }
+
+        // プレイヤーがつぶしエリアに入った場合
+        if (collision.transform.tag == "Player" && collision.gameObject == CharacterManager.Instance.PlayerObjSelf)
+        {
+            playerBase = collision.gameObject.GetComponent<PlayerBase>();   // つぶされ対象からPlayerBaseを取得
+
+            // プレイヤーの最大HP20%相当のダメージに設定
+            int damage = Mathf.FloorToInt(playerBase.MaxHP * 0.2f);
+            playerBase.ApplyDamage(damage);
+        }
+
+        // 敵がつぶしエリアに入った場合
+        if (collision.transform.tag == "Enemy")
+        {
+            enemyBase = collision.gameObject.GetComponent<EnemyBase>();   // つぶされ対象からEnemyBaseを取得
+
+            // 敵に大量のダメージを与えて、実質即死にする
+            int damage = 9999;
+
+            if ((RoomModel.Instance && RoomModel.Instance.IsMaster) || !RoomModel.Instance)
+            {
+                enemyBase.ApplyDamageRequest(damage, null, false, false);
+            }
         }
     }
 
     protected override void ApplyDamage()
     {
-        if (isBroken == true) return;
 
-        PlayerBase player = CharacterManager.Instance.PlayerObjSelf.GetComponent<PlayerBase>();
-        //破片オブジェクトを生成(position.xは箱の位置、yは箱より少し上の位置)
-        GameObject fragment = Instantiate(BoxFragment, new Vector2(this.transform.position.x, this.transform.position.y + 1.5f), this.transform.rotation);
-
-        for (int i = 0; i < fragment.transform.childCount; i++)
-        {//fragmentの子の数だけループ
-            if (this.transform.position.x - player.transform.position.x >= 0)
-            {//箱の位置と比べて、プレイヤーが左側にいたら
-                fragment.transform.GetChild(i).GetComponent<Rigidbody2D>().AddForce(new Vector2(1000, 200)); //右側に破片を飛ばす
-            }
-            else
-            {//箱の位置と比べて、プレイヤーが右側にいたら
-                fragment.transform.GetChild(i).GetComponent<Rigidbody2D>().AddForce(new Vector2(-1000, -200)); //左側に破片を飛ばす
-            }
-            FadeFragment(fragment.transform.GetChild(i));
-        }
-
-        //箱を壊す
-        Destroy(this.gameObject);
     }
 
     override public void DestroyFragment(Transform obj)
     {
-        if (obj == null) return;
-        Destroy(obj.gameObject);
+
     }
 
     /// <summary>
@@ -67,11 +73,7 @@ public class Box : ObjectBase
     /// <param name="fragment">破片</param>
     override public void FadeFragment(Transform fragment)
     {
-        if (fragment == null) return;
 
-        // 6秒かけて破片をフェードアウトさせ、その後破壊する
-        fragment.GetComponent<Renderer>().material.DOFade(0, 6).OnComplete(() =>
-        { DestroyFragment(fragment.transform);});
     }
 
     /// <summary>
@@ -79,8 +81,6 @@ public class Box : ObjectBase
     /// </summary>
     public override void TurnOnPower()
     {
-        // ダメージ付与
-        ApplyDamage();
         isBroken = true; // 破壊済みとする
     }
 }
