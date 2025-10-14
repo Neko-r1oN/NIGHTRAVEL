723f3ebb498bd91daa5aa6739d405be92433702d
Rui Enomoto <231013@st.yoshida-g.ac.jp>
2025-10-14 10:46:38 +0900

    ステージ３のザコ敵のステータスを弱体化

--- Client/Assets/99_User/02_Enomoto/02_Script/Enemies/Types/Stage2/Full Metal Worm/FullMetalWorm.cs
+++ Client/Assets/99_User/02_Enomoto/02_Script/Enemies/Types/Stage2/Full Metal Worm/FullMetalWorm.cs
@@ -383,7 +383,7 @@ public class FullMetalWorm : EnemyBase
             {
                 if (collidersEnemies[i].gameObject.tag == "Player")
                 {
-                    collidersEnemies[i].gameObject.GetComponent<PlayerBase>().ApplyDamage(power, transform.position,KB_POW.Big, applyEffect);
+                    collidersEnemies[i].gameObject.GetComponent<PlayerBase>().ApplyDamage(power * 10, transform.position,KB_POW.Big, applyEffect);
                 }
             }
             yield return null;
